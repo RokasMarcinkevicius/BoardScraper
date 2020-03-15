16a3f3ebe5d117328bceb45ca14f32884d2c73b9
Marcinkevičius Rokas <rokmar2@ktu.lt>
2020-03-15 22:33:03 +0200

    Read & Print

--- Board/Board/Board.py
+++ Board/Board/Board.py
@@ -4,3 +4,21 @@ import urllib.request, urllib.error # .request error handling library (Try, Exce
 import datetime
 import os
 import codecs
+
+page_url = ('https://boards.lt/en/home/10680-22836-nitro-2020-squash-snowboard.html')
+print(page_url)
+try: #2.Scraping (attempt to read)
+    conn = urllib.request.urlopen(page_url) #opened connection to page_url
+except urllib.error.HTTPError as e: #skip 404 pages
+    pass
+    # Return code error (e.g. 404, 501, ...)
+    #print('HTTPError: {}'.format(e.code))
+except urllib.error.URLError as e: #skip unrequestable pages
+    pass
+    # Not an HTTP-specific error (e.g. connection refused)
+    # print('URLError: {}'.format(e.reason))
+else:
+    # 200
+    page_soup = soup(conn.read(), "html.parser", from_encoding="utf-8")
+    conn.close() #closing open connection as page is read to memory
+    print(page_soup)
