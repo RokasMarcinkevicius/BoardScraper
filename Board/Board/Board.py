521efbf85d37b8fd5c924577f7eadc1e93daf71c
Marcinkevičius Rokas <rokmar2@ktu.lt>
2020-03-15 22:24:18 +0200

    Init

---
+++ Board/Board/Board.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup as soup  # HTML data structure
+from urllib.request import urlopen as uReq  # Web client request
+import urllib.request, urllib.error # .request error handling library (Try, Except)
+import datetime
+import os
+import codecs
